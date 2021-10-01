ea05843ce96cb3cfc2d3ba3d8afb5a68993f356d
Jon Ronen-Drori <jon_ronen@yahoo.com>
2021-10-01 15:47:05 +0300

    First Commit

---
+++ bullshit_action.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+import time
+import thread
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print "Usage: %s DIRECTION" % sys.argv[0]
+    sys.exit(-1)
+
+direction = sys.argv[1].lower().strip()
+
+if direction not in ["up", "down"]:
+    print "Unsupported direction %s, please choose up or down" % sys.argv[1]
+    sys.exit(-1)
+
+mot_ctrl = open ("/sys/class/gpio/gpio16/value", "wt")
+
+x_step = open ("/sys/class/gpio/gpio5/value", "wt")
+x_dir = open ("/sys/class/gpio/gpio6/value", "wt")
+
+if direction == "up":
+    print "up"
+    x_dir.write ("0\n")
+    os.system("aplay -D bluealsa:DEV=B8:D5:0B:B2:31:5C,PROFILE=a2dp bullshit.wav &")
+else:
+    print "down"
+    x_dir.write ("1\n")
+x_dir.flush ()
+mot_ctrl.write ("0\n")
+mot_ctrl.flush ()
+
+steps = 0
+
+try:
+    while steps < 1000:
+        # first pause for setting the step high
+        time.sleep (0.001)
+
+        x_step.write ("1\n")
+        x_step.flush ()
+
+        # now pause for setting the step low
+        time.sleep (0.001)
+
+        x_step.write ("0\n")
+        x_step.flush ()
+
+        steps += 1
+except KeyboardInterrupt:
+    pass
+
+mot_ctrl.write ("1\n")
+mot_ctrl.flush ()
+
---
+++ bullshit_start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+/bin/echo 5 > /sys/class/gpio/export 
+/bin/echo 6 > /sys/class/gpio/export 
+/bin/echo 12 > /sys/class/gpio/export 
+/bin/echo 13 > /sys/class/gpio/export 
+/bin/echo 16 > /sys/class/gpio/export 
+/bin/echo 19 > /sys/class/gpio/export 
+/bin/echo 21 > /sys/class/gpio/export 
+/bin/echo 23 > /sys/class/gpio/export 
+/bin/echo 26 > /sys/class/gpio/export 
+
+/bin/sleep 1
+
+/bin/echo "out" > /sys/class/gpio/gpio5/direction 
+/bin/echo "out" > /sys/class/gpio/gpio6/direction 
+/bin/echo "out" > /sys/class/gpio/gpio12/direction 
+/bin/echo "out" > /sys/class/gpio/gpio13/direction 
+/bin/echo "out" > /sys/class/gpio/gpio16/direction 
+/bin/echo "out" > /sys/class/gpio/gpio19/direction 
+/bin/echo "out" > /sys/class/gpio/gpio21/direction 
+/bin/echo "in" > /sys/class/gpio/gpio23/direction 
+/bin/echo "out" > /sys/class/gpio/gpio26/direction 
+
+/bin/sleep 1
+
+echo -e "power up\nconnect b8:d5:0b:b2:31:5c\n quit"|bluetoothctl
+
+/bin/sleep 10
+
