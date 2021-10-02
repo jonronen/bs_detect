9ad133c4bbd1952fbb0f9f2cbe12ae93428f6974
Jon Ronen-Drori <jon_ronen@yahoo.com>
2021-10-02 10:30:24 +0300

    update buzzword list

--- buzzwords.py
+++ buzzwords.py
@@ -6,6 +6,15 @@ buzzword_list = [
     "win", "strategy", "quantum", "team", "deadline", "open", "server",
     "serverless", "fabulous", "delivery", "deliver", "leadning",
     "cost", "continuous", "edge", "multi", "disciplinary", "integrated",
-    "algorithm"
+    "algorithm", "analytics", "deliverables", "analytic", "commerce",
+    "product", "metric", "customer", "customers", "oriented", "orientation",
+    "scale", "scaling", "scalable", "effective", "target", "targeted",
+    "social", "engineering", "media", "engineer", "data", "driven", "brand",
+    "branding", "transformation", "transform", "gamification", "ideation",
+    "box", "creative", "patent", "patents", "organic", "personalization",
+    "integration", "real", "time", "repurposing", "targeting", "leader",
+    "leading", "growth", "growing", "shiny", "internet", "issue",
+    "optimization", "optimized", "optimize", "optimum", "organization",
+    "organizational"
 ]
 
