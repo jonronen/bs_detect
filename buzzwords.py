cc262346fb81acdbd08b774180fc3289dd074448
Jon Ronen-Drori <jon_ronen@yahoo.com>
2021-10-02 10:02:53 +0300

    Adding buzzwords for bullshit detection

---
+++ buzzwords.py
@@ -0,0 +1,11 @@
+buzzword_list = [
+    "banana", "machine", "learning", "artificial",
+    "intelligence", "smart", "cloud", "deep", "blockchain",
+    "amazing", "wonderful", "super", "crypto", "encryption", "encrypted",
+    "agile", "robust", "interdisciplinary", "over", "killer", "winning",
+    "win", "strategy", "quantum", "team", "deadline", "open", "server",
+    "serverless", "fabulous", "delivery", "deliver", "leadning",
+    "cost", "continuous", "edge", "multi", "disciplinary", "integrated",
+    "algorithm"
+]
+
